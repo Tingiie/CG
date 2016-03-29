8c8dcea77063849a59d8a5c58339647fb5045162
tingiie <fatima.a.alpha@gmail.com>
2016-03-29 13:51:07 +0200

    created main method, not fully implemented

--- src/HumectantChecker.java
+++ src/HumectantChecker.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 
 public class HumectantChecker {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insert ingredients to check if it's a humectant");
+		String ingredient = sc.nextLine();
+		sc.close();
+		
+	}
 }
