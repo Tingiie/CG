327323e9845922997ef528b42674004b3a99d22e
tingiie <fatima.a.alpha@gmail.com>
2016-03-31 00:01:13 +0200

    Implemented a loop for the ingredient input

--- src/HumectantChecker.java
+++ src/HumectantChecker.java
@@ -6,15 +6,20 @@ public class HumectantChecker {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Insert ingredients to check if it's a humectant");
-		String ingredient = sc.nextLine();
-		sc.close();
-		ingredient = ingredient.toLowerCase();
-		File file = new File("Humectants.txt");
-		Parser parser = new Parser(file);
-		ArrayList<String> humectants = parser.parse();
-		
-		if (humectants.contains(ingredient)){
-			System.out.println("This ingredient is a humectant: " + ingredient);
-		}	
+
+		while (true) {
+			String ingredient = sc.nextLine();
+			ingredient = ingredient.toLowerCase();
+			File file = new File("Humectants.txt");
+			Parser parser = new Parser(file);
+			ArrayList<String> humectants = parser.parse();
+
+			if (humectants.contains(ingredient)) {
+				System.out.println("This ingredient is a humectant: " + ingredient);
+			} else {
+				System.out.println(ingredient + " is not a humectant");
+
+			}
+		}
 	}
 }
